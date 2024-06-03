8a284f32cd6e2dfbe2494c2d5b4c0160bd58cf3c
Ferolivera12 <fer.oliveravel12@gmail.com>
2024-06-03 12:03:48 -0600

    feat: Se creo una pestaña Inicio

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
-//import './customcss/style.css'
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import Home from './components/Home';
 import ShowMedicamentos from './components/ShowMedicamentos';
 import CreateMedicamento from './components/CreateMedicamento';
-//import EditMedicamento from './components/EditMedicamento';
+// import EditMedicamento from './components/EditMedicamento';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<ShowMedicamentos />} />
-          <Route path='/create' element={<CreateMedicamento/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/medicamentos" element={<ShowMedicamentos />} />
+          <Route path="/medicamentos/create" element={<CreateMedicamento />} />
+          {/* <Route path="/medicamentos/edit/:id" element={<EditMedicamento />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
