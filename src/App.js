91774bc21a29937705c45050b6c7844c39584e3d
Edgardo <jesus.2002.04.12@gmail.com>
2024-06-03 09:03:40 -0600

    errores

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+//import './customcss/style.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import ShowMedicamentos from './components/Medicamento/ShowMedicamentos';
 import CreateMedicamento from './components/Medicamento/CreateMedicamento';
 import EditMedicamento from './components/Medicamento/EditMedicamento';
-import 
 
 function App() {
   return (
@@ -14,8 +14,6 @@ function App() {
           <Route path='/' element={<ShowMedicamentos />} />
           <Route path='/create' element={<CreateMedicamento />} />
           <Route path='/edit/:id' element={<EditMedicamento />} />
-          <Route path='/login' element={<Login />} />
-
         </Routes>
       </BrowserRouter>
     </div>
