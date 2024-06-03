cea961a1bc65fcf2f0b1ec9f557f94b711c08845
Edgardo <jesus.2002.04.12@gmail.com>
2024-06-03 08:55:33 -0600

    correccion

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ShowMedicamentos from './components/Medicamento/ShowMedicamentos';
 import CreateMedicamento from './components/Medicamento/CreateMedicamento';
 import EditMedicamento from './components/Medicamento/EditMedicamento';
+import 
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
           <Route path='/' element={<ShowMedicamentos />} />
           <Route path='/create' element={<CreateMedicamento />} />
           <Route path='/edit/:id' element={<EditMedicamento />} />
+          <Route path='/login' element={<Login />} />
+
         </Routes>
       </BrowserRouter>
     </div>
